682c2766318659913311aaa700b7aa820b0c7823
MiguelFPP <miguelferneyp@gmai.com>
2022-11-07 11:42:08 -0500

    fix

--- tests/Feature/CandidateTest.php
+++ tests/Feature/CandidateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Candidate;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -97,5 +98,44 @@ class CandidateTest extends TestCase
         ]);
 
         $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function can_see_one_candidate(){
+        $user=factory(User::class)->create([
+            'username' => 'user',
+            'password' => bcrypt('password'),
+            'role' => 'manager',
+        ]);
+
+        $agent=factory(User::class)->create([
+            'username' => 'agent',
+            'password' => bcrypt('password'),
+            'role' => 'agent',
+        ]);
+
+        $response = $this->actingAs($user)->post('/api/lead',[
+            'name' => 'test',
+            'source' => 'test',
+            'user_id' => 'test',
+            'created_by' => $agent->id,
+        ]);
+
+        $candidate=Candidate::first();
+
+        $response = $this->actingAs($user)->get('/api/lead/'.$candidate->id);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            "meta"=>[
+                "success",
+            ],
+            "data"=>[
+                "name",
+                "source",
+                "user_id",
+                "created_by",
+            ],
+        ]);
     }
 }
